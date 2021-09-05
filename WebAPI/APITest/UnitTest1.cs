37a665418899ceb0756aa6aa0aee1413f40c11cd
skullwarriorsergio <73899511+skullwarriorsergio@users.noreply.github.com>
2021-09-04 21:55:54 -0400

    Adicionando pruebas al WebAPI

--- WebAPI/APITest/UnitTest1.cs
+++ WebAPI/APITest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using WebAPI.Controllers;
 using Xunit;
 
 namespace APITest
@@ -16,13 +17,44 @@ namespace APITest
         #endregion
 
         [Fact]
-        public void Test1()
+        public async void NotasTest()
         {
             using (var transaction = Fixture.Connection.BeginTransaction())
             {
                 using (var context = Fixture.CreateContext(transaction))
-                {
+                {          
+                    //Probando "Estudiantes"
+                    var controllerEst = new EstudiantesController(context);
+                    _ = await controllerEst.PostEstudiante(new WebAPI.Models.EstudiantePOST
+                    {
+                        Nombre = "Lizbet"
+                    });
+                    Assert.Single(controllerEst.GetEstudiante().Result.Value);
+                    var estudiante = controllerEst.GetEstudiante().Result.Value.First();
 
+                    var controllerProf = new ProfesoresController(context);
+                    _ = await controllerProf.PostProfesor(new WebAPI.Models.ProfesorPOST
+                    {
+                        Nombre = "Sergio"
+                    });
+                    _ = await controllerProf.PostProfesor(new WebAPI.Models.ProfesorPOST
+                    {
+                        Nombre = "Kevin"
+                    });
+                    Assert.Equal(2, controllerProf.GetProfesor().Result.Value.Count());
+                    var profesor= controllerProf.GetProfesor().Result.Value.Last();
+
+                    var controller = new NotasController(context);
+                    _ = controller.PostNota(new WebAPI.Models.NotaPOST
+                    {
+                        EstudianteID = estudiante.EstudianteID,
+                        ProfesorID = profesor.ProfesorID,
+                        Nombre = "Matemáticas",
+                        Valor = 5
+                    });
+                    var result = controller.GetNota().Result;
+                    Assert.Single(result.Value);
+                    Assert.Equal(5, result.Value.First().Valor);
                 }
             }
         }
